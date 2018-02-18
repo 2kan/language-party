900455ed3c47c123558c924e3c6b167c80e7cc7b
Tom Penney <dev@2kan.io>
2018-02-18 22:44:42 +1100

    Language codes are now sent by the API request

--- app.js
+++ app.js
@@ -7,29 +7,46 @@ app.use( bodyParser.urlencoded( { extended: false } ) );
 app.use( bodyParser.json() );
 
 const gapi = require( "./google-api.json" );
+const langMap = require( "./languages.json" );
 
 const TRANSLATE_URL = "https://translation.googleapis.com/language/translate/v2";
 
-const languages = [ "vi", "en" ];
+//const languages = [ "zu", "en" ];
 
 app.post( "/", function ( a_req, a_res )
 {
 	console.log( "." );
-	if ( a_req.body[ "text" ] == undefined )
+	if ( a_req.body[ "text" ] == undefined || a_req.body[ "languages" ] == undefined )
 	{
-		a_res.send( "no input" );
+		a_res.status( 400 ).send( "missing input" ); // todo: make this mor informative
 		return;
 	}
 
-	var thisTranslation = undefined;
-	var lastEnglish = undefined;
-	var translations = [];
-	var langIndex = 0;
+	var languages = a_req.body[ "languages" ].replace( /\s/g, "" ).split( "," );
+	if ( languages.length == 0 )
+	{
+		a_res.status( 400 ).send( "invalid languages input" );
+		return;
+	}
 
-	translate( 0, a_req.body[ "text" ], [], a_res );
+	// Validate input languages
+	var humanLangs = [];
+	for ( var i = 0; i < languages.length; ++i )
+	{
+		if ( langMap[ languages[ i ] ] == undefined )
+		{
+			a_res.status( 400 ).send( "invalid language: " + languages[ i ] );
+			return;
+		}
+
+		humanLangs.push( langMap[ languages[ i ] ] );
+	}
+
+	console.log( "Translating through: " + humanLangs.join( " -> " ) );
+	translate( languages, 0, a_req.body[ "text" ], [], a_res );
 } );
 
-function translate( a_langIndex, a_textToTranslate, a_translations, a_httpResponse )
+function translate( a_languages, a_langIndex, a_textToTranslate, a_translations, a_httpResponse )
 {
 	if ( a_translations.includes( a_textToTranslate ) )
 	{
@@ -46,9 +63,9 @@ function translate( a_langIndex, a_textToTranslate, a_translations, a_httpRespon
 		url: TRANSLATE_URL,
 		form: {
 			q: a_textToTranslate,
-			target: languages[ a_langIndex % languages.length ],
+			target: a_languages[ a_langIndex % a_languages.length ],
 			format: "text",
-			source: languages[ a_langIndex - 1 % languages.length ],
+			source: a_languages[ a_langIndex - 1 % a_languages.length ],
 			model: "base",
 			key: gapi.api_key
 		}
@@ -56,27 +73,29 @@ function translate( a_langIndex, a_textToTranslate, a_translations, a_httpRespon
 		{
 			if ( a_err )
 			{
-				a_httpResponse.status( 500 ).send( "Google done and dun goofed :/" )( a_err );
+				a_httpResponse.status( 500 ).send( "Something gone and done goofed :/" )( a_err );
 				return;
 			}
+
+			// TODO: check if there is an error code in google's response object
 
 			const body = JSON.parse( a_body );
 
 			if ( body == undefined || body.data == undefined || body.data.translations == undefined )
 			{
-				a_httpResponse.status( 500 ).send( "Something did gone and dun goofed :/" );
+				a_httpResponse.status( 500 ).send( "Unexpected response from Google Translate API" );
 				return;
 			}
 
 			//console.log( body.data.translations[ 0 ].translatedText );
 
-			if ( a_langIndex % languages.length == 0 )
+			if ( a_langIndex % a_languages.length == 0 )
 			{
 				a_translations.push( a_textToTranslate );
 				//console.log( "added " + a_textToTranslate + " to translated array." );
 			}
 
-			translate( a_langIndex + 1, body.data.translations[ 0 ].translatedText, a_translations, a_httpResponse );
+			translate( a_languages, a_langIndex + 1, body.data.translations[ 0 ].translatedText, a_translations, a_httpResponse );
 		}
 	);
 }
