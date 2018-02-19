bfb0ec23387d1db8895b07685c2a74022b81b9c2
Tom Penney <dev@2kan.io>
2018-02-19 19:28:15 +1100

    Fancy new JS to interact with the fancy new UI

--- public/js/translate.js
+++ public/js/translate.js
@@ -1,19 +1,33 @@
+const DEFAULT_LANGUAGE = "en";
+const translationTemplate = "<div class='languageCard ~equilibrium~'><div class='content'><h1 class='translation'>~translation~</h1><div class='meta'>Translated to <span class='language'>~language~</span></div></div></div>";
+var langMap = [];
+
+fetch( "/js/languages.json" )
+	.then( response => response.json() )
+	.then( json =>
+	{
+		langMap = json;
+		LoadLanguages();
+	} );
+
 $( document ).ready( function ()
 {
 	$( "#inputForm" ).on( "submit", function ()
 	{
-		$( "#translationOutlet" ).html( "" );
-		socket.emit( "translate", { text: $( "#input" ).val(), languages: "zu,ar,th,zh-TW,lb,uk,en" } );
-		return false;
+		Submit();
+		return false; // prevent default form submission behaviour
 	} );
+
+	$( "#submit" ).on( "click", Submit );
 } );
 
 socket.on( "translation", function ( a_response )
 {
 	console.log( a_response );
-	var html = "<strong>" + a_response.language + "</strong> - " + a_response.translation + "<br />";
-	if ( a_response.equilibrium )
-		html = "<span style='color:green'>" + html + "</span>";
+	var html = translationTemplate
+		.replace( "~translation~", a_response.translation )
+		.replace( "~language~", a_response.language )
+		.replace( "~equilibrium~", ( a_response.equilibrium ) ? "equilibrium" : "" );
 
 	$( "#translationOutlet" ).append( html );
 } );
@@ -23,3 +37,30 @@ socket.on( "err", function ( a_err )
 	console.log( a_err );
 	$( "#translationOutlet" ).append( "<strong>" + a_err + "</strong>" );
 } );
+
+
+
+function Submit()
+{
+	// Clear last set of translations, if any
+	$( "#translationOutlet" ).html( "" );
+
+	socket.emit( "translate", {
+		text: $( "#inputPhrase" ).val(),
+		languages: "th,en"
+	} );
+}
+
+function LoadLanguages()
+{
+	var languageOptions = "";
+	for ( var key in langMap )
+	{
+		if ( langMap.hasOwnProperty( key ) )
+			languageOptions += "<option value='" + key + "'>" + langMap[ key ] + "</option>";
+	}
+
+	$( "#sourceLanguage, #otherLanguage" ).append( languageOptions );
+	$( "#sourceLanguage" ).val( DEFAULT_LANGUAGE );
+	$("#otherLanguage")
+}
