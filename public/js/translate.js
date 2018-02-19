b6ad406eb05a4a398bc9b18f000d603c17552a14
Tom Penney <dev@2kan.io>
2018-02-19 19:58:09 +1100

    UI can no longer submit >1 req at a time, added random lang setter

--- public/js/translate.js
+++ public/js/translate.js
@@ -1,7 +1,9 @@
 const DEFAULT_LANGUAGE = "en";
 const translationTemplate = "<div class='languageCard ~equilibrium~'><div class='content'><h1 class='translation'>~translation~</h1><div class='meta'>Translated to <span class='language'>~language~</span></div></div></div>";
 var langMap = [];
+var submissionLocked = false;
 
+// Fetch the language map and load the languages into select elements
 fetch( "/js/languages.json" )
 	.then( response => response.json() )
 	.then( json =>
@@ -9,6 +11,7 @@ fetch( "/js/languages.json" )
 		langMap = json;
 		LoadLanguages();
 	} );
+
 
 $( document ).ready( function ()
 {
@@ -29,6 +32,9 @@ socket.on( "translation", function ( a_response )
 		.replace( "~language~", a_response.language )
 		.replace( "~equilibrium~", ( a_response.equilibrium ) ? "equilibrium" : "" );
 
+	if ( a_response.equilibrium )
+		UnlockSubmission();
+
 	$( "#translationOutlet" ).append( html );
 } );
 
@@ -42,18 +48,28 @@ socket.on( "err", function ( a_err )
 
 function Submit()
 {
+	if ( submissionLocked )
+		return;
+
+	LockSubmission();
+
 	// Clear last set of translations, if any
 	$( "#translationOutlet" ).html( "" );
 
+	var sourceLanguage = $( "#sourceLanguage" ).val();
+	var otherLanguage = $( "#otherLanguage" ).val();
+
 	socket.emit( "translate", {
 		text: $( "#inputPhrase" ).val(),
-		languages: "th,en"
+		languages: otherLanguage + "," + sourceLanguage
 	} );
 }
 
 function LoadLanguages()
 {
 	var languageOptions = "";
+
+	// Create the HTML for the language select elements
 	for ( var key in langMap )
 	{
 		if ( langMap.hasOwnProperty( key ) )
@@ -62,5 +78,25 @@ function LoadLanguages()
 
 	$( "#sourceLanguage, #otherLanguage" ).append( languageOptions );
 	$( "#sourceLanguage" ).val( DEFAULT_LANGUAGE );
-	$("#otherLanguage")
+	$( "#otherLanguage" ).val( GetRandomLanguage() );
+}
+
+function LockSubmission()
+{
+	submissionLocked = true;
+	$( "#submit" ).prop( "disabled", true );
+}
+
+function UnlockSubmission()
+{
+	submissionLocked = false;
+	$( "#submit" ).prop( "disabled", false );
+}
+
+function GetRandomLanguage()
+{
+	var langCodes = Object.keys( langMap );
+	var randomIndex = Math.floor( Math.random() * langCodes.length );
+
+	return langCodes[ randomIndex ];
 }
